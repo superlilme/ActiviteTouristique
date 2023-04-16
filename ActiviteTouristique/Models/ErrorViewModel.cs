0356de2f9ab752f35a9d5f73c711b5a9ef5a471d
superlilme <salmycruzesco@gmail.com>
2023-04-16 12:25:00 -0400

    Add project files.

---
+++ ActiviteTouristique/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ActiviteTouristique.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
